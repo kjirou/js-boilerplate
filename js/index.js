cfbe47563f39fac0bfd540dd2fdbb6e2d66781f4
kjirou <kjirou.web@gmail.com>
2015-03-09 03:17:55 +0900

    Add more info

--- js/index.js
+++ js/index.js
@@ -7,12 +7,18 @@ console.log(React.toString());
 
 
 //
-// browserify のたぶんバグの再現
+// browserify の変に見える挙動の再現
 //
 // 下のコードは正常に動くが
-// 最初の false ブロック内の require を削除すると
-// Cannot find module './required-data'
-// のエラーになる
+// 最初の false ブロック内の require を削除すると、
+// 実行時に Cannot find module './required-data' のエラーになる
+//
+// これはそもそも限られたファイルを事前に結合する仕様上、
+// 明示されていないパスのソースは読み込まない、たぶん仕様。
+//
+// https://github.com/substack/node-browserify/issues/883
+//
+// どちらかというと、これがそれを解決するハックっぽい
 //
 if (false) {
   require('./required-data');
