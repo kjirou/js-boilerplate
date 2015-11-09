d1dbb2470e769041be07364b4c322d5dfee5cff7
kjirou <kjirou.web@gmail.com>
2015-11-10 02:09:03 +0900

    Define `postcss` task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var babelify = require('babelify');
 var browserify = require('browserify');
 var gulp = require('gulp');
-var gulpPostcss = require('gulp-postcss');
 var gulpRename = require('gulp-rename');
+var gulpPostcss = require('gulp-postcss');
 var licensify = require('licensify');
 var notifier = require('node-notifier');
 var path = require('path');
@@ -24,6 +24,7 @@ var SRC_ROOT = path.join(ROOT, 'src');
 var PUBLIC_ROOT = path.join(ROOT, 'public');
 var PUBLIC_DIST_ROOT = path.join(PUBLIC_ROOT, 'dist');
 var SRC_FILE_PATH = path.join(SRC_ROOT, 'index.js');
+var CSS_INDEX_FILE_PATH = path.join(SRC_ROOT, 'styles/index.css');
 
 
 function createBundler(options) {
@@ -110,6 +111,20 @@ gulp.task('watch:js', function() {
   });
 });
 
+gulp.task('postcss', function() {
+  return gulp.src(CSS_INDEX_FILE_PATH)
+    .pipe(gulpPostcss([
+      require('autoprefixer')(),
+      require('postcss-custom-properties')(),
+      require('postcss-sassy-mixins')()
+    ], {
+      syntax: require('postcss-scss')
+    }))
+    .pipe(gulpRename('app.css'))
+    .pipe(gulp.dest(PUBLIC_DIST_ROOT))
+  ;
+});
+
 gulp.task('serve', function() {
   browserSync.init({
     server: {
@@ -148,66 +163,4 @@ gulp.task('develop', ['watch:js', 'serve']);
 //    })
 //    .pipe(gulpRename('styles.css'))
 //    .pipe(gulp.dest('./public'));
-//});
-//
-//gulp.task('postcss', function() {
-//  return browserify('./es6/index.es6', {
-//      debug: true,
-//      extensions: ['.es6']
-//    })
-//    .transform(babelify)
-//    .bundle()
-//    .pipe(vinylSourceStream('bundle.js'))
-//    .pipe(gulp.dest('./public'))
-//  ;
-//});
-//
-//function onPostcssSass(options) {
-//  var stream = new require('stream').Transform({objectMode: true});
-//  stream._transform = function(file, encoding, cb) {
-//    if (file.isNull()) {
-//      return cb(null, file);
-//    }
-//    if (file.isStream()) {
-//      return cb(new Error('gulp-postcss-scss', 'ERROR!!'));
-//      //return cb(new PluginError('gulp-postcss-scss', error));
-//    } else if (file.isBuffer()) {
-//      try {
-//        var result = nano.process(String(file.contents), assign(options, {
-//            map: (file.sourceMap) ? {annotation: false} : false,
-//            from: file.relative,
-//            to: file.relative
-//        }));
-//        if (result.map && file.sourceMap) {
-//            applySourceMap(file, String(result.map));
-//            file.contents = new Buffer(result.css);
-//        } else {
-//            file.contents = new Buffer(result);
-//        }
-//
-//        this.push(file);
-//      } catch (e) {
-//        var p = new PluginError(PLUGIN_NAME, e, {fileName: file.path});
-//        this.emit('error', p);
-//      }
-//        cb();
-//      }
-//    }
-//  };
-//  return stream;
-//}
-//
-//gulp.task('postcss', function() {
-//  return gulp.src('./postcss/index.css')
-//    .pipe(gulpPostcss([
-//      //require('autoprefixer-core')(),
-//      //require('postcss-custom-properties')()
-//      //require('postcss-sassy-mixins')()
-//      //require('cssnano')()
-//    ], {
-//      //syntax: require('postcss-scss')
-//    }))
-//    .pipe(gulpRename('postcss.css'))
-//    .pipe(gulp.dest('./public'))
-//  ;
 //});
