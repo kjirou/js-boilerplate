5b2da3cd69bead030c67ab7723c8e108e9d8e3f1
kjirou <kjirou.web@gmail.com>
2015-12-26 17:03:19 +0900

    Create "components" dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Root from './Root';
+import Root from './components/Root';
 
 
 window.document.addEventListener('DOMContentLoaded', function() {
