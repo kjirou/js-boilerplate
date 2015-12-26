1cf3989bf46aa6905a8738220cc47dd72f7502e1
kjirou <kjirou.web@gmail.com>
2015-12-26 17:04:37 +0900

    Rewrite to modern ES syntax

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/Root';
 
 
-window.document.addEventListener('DOMContentLoaded', function() {
-  var $container = window.document.querySelector('#js-app-container');
-  ReactDOM.render(React.createElement(Root), $container);
+window.document.addEventListener('DOMContentLoaded', () => {
+  const container = window.document.querySelector('#js-app-container');
+  ReactDOM.render(React.createElement(Root), container);
 });
