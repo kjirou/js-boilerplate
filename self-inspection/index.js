fbb0d3495f2253b494f10512a072d2b33fb4a8a8
kjirou <kjirou.web@gmail.com>
2016-01-30 03:27:37 +0900

    Give up testing for development tasks

--- self-inspection/index.js
+++ self-inspection/index.js
@@ -3,7 +3,7 @@ import childProcess from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import rimraf from 'rimraf';
-import webdriver, { until } from 'selenium-webdriver';
+//import webdriver, { until } from 'selenium-webdriver';
 
 
 const PUBLIC_DIST_ROOT = path.join(__dirname, '../public/dist');
@@ -11,35 +11,12 @@ const APP_JS_FILE_PATH = path.join(PUBLIC_DIST_ROOT, 'app.js');
 const APP_CSS_FILE_PATH = path.join(PUBLIC_DIST_ROOT, 'app.css');
 
 
-export const buildDriver = () => {
-  const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-  return driver.manage().window().setSize(1024, 768)
-    .then(() => webdriver.promise.fulfilled(driver));
-};
-
 export const deleteDistDir = (callback) => {
   rimraf(PUBLIC_DIST_ROOT, callback);
 };
 
 
 describe('self inspection', () => {
-
-  //let driver;
-
-  //before(done => {
-  //  buildDriver()
-  //    .then(newDriver => {
-  //      driver = newDriver;
-  //      done();
-  //    }, err => done(err));
-  //});
-
-  //after(done => {
-  //  driver.quit()
-  //    .then(() => done(), err => done(err));
-  //});
 
   beforeEach(done => {
     deleteDistDir(done);
@@ -102,4 +79,62 @@ describe('self inspection', () => {
       });
     });
   });
+
+
+  // TODO: Not yet running. In addition, it is also very unstable as moved if.
+  //describe('development', () => {
+  //
+  //  let driver;
+  //  let develop;
+  //  let url;
+  //
+  //  export const buildDriver = () => {
+  //    const driver = new webdriver.Builder()
+  //      .forBrowser('chrome')
+  //      .build();
+  //    return driver.manage().window().setSize(1024, 768)
+  //      .then(() => webdriver.promise.fulfilled(driver));
+  //  };
+  //
+  //  before(done => {
+  //    buildDriver()
+  //      .then(newDriver => {
+  //        driver = newDriver;
+  //        done();
+  //      }, err => done(err));
+  //  });
+  //
+  //  before(done => {
+  //    develop = childProcess.spawn('npm', ['run', 'develop']);
+  //    develop.stdout.on('data', (data) => {
+  //      const line = data.toString();
+  //      const matched = line.match(/Local: (http:\/\/localhost:\d+)/);
+  //      if (matched) {
+  //        url = matched[1];
+  //        setTimeout(done, 5000);
+  //      }
+  //    });
+  //  });
+  //
+  //  after(() => {
+  //    develop.kill('SIGKILL');
+  //  });
+  //
+  //  after(done => {
+  //    driver.quit()
+  //      .then(() => done(), err => done(err));
+  //  });
+  //
+  //  it('should be', done => {
+  //    webdriver.promise.fulfilled()
+  //      .then(() => driver.get(url))
+  //      .then(() => driver.getPageSource())
+  //      .then((html) => console.log(html))
+  //      .then(() => driver.wait(until.elementLocated({ css: 'root' }), 10000))
+  //      .then(el => el.getInnerHtml())
+  //      .then((html) => console.log(html))
+  //      .then(done, err => done(err))
+  //    ;
+  //  });
+  //});
 });
