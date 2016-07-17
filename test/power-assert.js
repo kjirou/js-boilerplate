f5be94976675a660d6c57bff6f9d3215eed97e8b
kjirou <kjirou.web@gmail.com>
2016-07-17 17:55:14 +0900

    Try to reproduce a power-assert bug in another project (, but it is not success)

--- test/power-assert.js
+++ test/power-assert.js
@@ -1,7 +1,32 @@
 import assert from 'power-assert';
 
+import PowerAssertTester from '../src/lib/PowerAssertTester';
+
 
 describe('power-assert', () => {
+
+  //it('is not working?', () => {
+  //  // OK
+  //  //assert.strictEqual([1, 2].length, 3);
+
+  //  // OK
+  //  //const ary = [1, 2];
+  //  //assert.strictEqual(ary.length, 3);
+
+  //  // OK
+  //  //class Foo {
+  //  //  constructor() {
+  //  //    this.ary = [1, 2];
+  //  //  }
+  //  //}
+  //  //const foo = new Foo();
+  //  //assert.strictEqual(foo.ary.length, 3);
+
+  //  // OK
+  //  //const tester = new PowerAssertTester();
+  //  //tester._ary = [1, 2];
+  //  //assert.strictEqual(tester._ary.length, 3);
+  //})
 
   it('is enabled', () => {
     const actual = {
