d896fe2547853d8da2cc45106c850aeed1394b71
kjirou <kjirou.web@gmail.com>
2016-12-17 14:05:36 +0900

    WIP

---
+++ src/foo.js
@@ -0,0 +1,3 @@
+export const x = 1;
+export const y = 2;
+export default { a: 11, b: 12 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 import Root from './components/Root';
 
+import foo, { x, y, a, b } from './foo';
+
+console.log(foo);
+console.log(x);
+console.log(y);
+console.log(a);
+console.log(b);
+
 
 window.document.addEventListener('DOMContentLoaded', () => {
   const container = window.document.querySelector('.js-app-container');
